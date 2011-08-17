6a36f6b7c7fb7304f66c7c287dce7939d04e6205
Dominik Piekarski <dompie@matasia.de>
2011-08-17 10:56:14 +0200

    ENH: Language switcher works now with subsites together FIX: Language switch before IE 9

--- javascript/TranslatableModelAdmin.js
+++ javascript/TranslatableModelAdmin.js
@@ -1,21 +1,22 @@
 (function($) {
 $(document).ready(function() {
 
-	$('#Form_EditForm_createtranslation').live('click', function() {
-		var url = $('#Form_EditForm').attr('action') 
+	$('#Form_EditForm_createtranslation').livequery('click', function() {
+		var url = $('#Form_EditForm').attr('action')
 			+ '?action_createtranslation=1'
-			+ '&newlang=' + $('#Form_EditForm_NewTransLang').val()
-			+ '&SecurityID=' + $('#Form_EditForm_SecurityID').val()
-			
+			+ '&newlang=' + $('#Form_EditForm_NewTransLang').val();
+		if('$SECURITY_ID' != '')
+			url += '&SecurityID=' + '$SECURITY_ID';
+
 		$('#ModelAdminPanel').fn('loadForm', url);
-		
+
 		return false;
 	});
 
 
 	var _TRANSLATING_LANG = null;
-	$('#LangSelector').change(function() {
-		document.location = SiteTreeHandlers.controller_url + '?locale=' + this.value;
+	$('#LangSelector').livequery('change', function() {
+		document.location.search = '?locale=' + this.value;
 	});
 })
 })(jQuery);
